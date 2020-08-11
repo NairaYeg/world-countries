73dca4f33c7f7750089abf2563440f655247413d
Naira Yeghiazaryan <yeghiazaryannaira97@gmail.com>
2020-08-11 14:19:04 +0400

    add bookmark icons

--- js/helpers/createTableRow.js
+++ js/helpers/createTableRow.js
@@ -1,19 +1,29 @@
 import {tableBody} from "../script.js"
+// import {bookmarkIcon} from "../script.js"
+
 
 export function createTableRow({name, flag, nativeName}){
     const newRow = tableBody.insertRow(tableBody.length)
 
+   
     const nameCell = newRow.insertCell(0)
     const flagCell = newRow.insertCell(1)
 
+    let bookmarkIcon = document.createElement("i")
     let img = document.createElement("img")
     let countryName = document.createElement("h3")
-
+    
+    countryName.style.padding = "10px"
     countryName.innerText = name;
-    nameCell.append(countryName)
-
     img.src = flag;
     img.classList.add("flag-img")
+    bookmarkIcon.classList.add("far")
+    bookmarkIcon.classList.add("fa-bookmark")
+    bookmarkIcon.classList.add("fa-3x")
+    nameCell.classList.add("bookmarkIcon")
+
+    nameCell.appendChild(countryName)
+    nameCell.appendChild(bookmarkIcon)
     flagCell.append(img)
 
     newRow.classList.add("country")
--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@ import {BASE_URL} from "./helpers/constants.js"
 import {createTableRow} from "./helpers/createTableRow.js"
 
 export const tableBody = document.querySelector("tbody")
+// export const bookmarkIcon = document.querySelector(".fa-bookmark")
 
 
 doGet("https://restcountries.eu/rest/v2/all")
 .then(countries => {
     countries.forEach((country) => {
         createTableRow(country)
-        console.log(country)
     });
 })
