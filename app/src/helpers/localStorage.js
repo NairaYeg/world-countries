9877b12fe9140796a8c89629fdb2bad77384040d
Naira Yeghiazaryan <yeghiazaryannaira97@gmail.com>
2020-08-19 23:40:45 +0400

    add getItemFromLocalStorage() removeItemFromLocalStorage() functions

--- app/src/helpers/localStorage.js
+++ app/src/helpers/localStorage.js
@@ -1,5 +1,5 @@
 /**
- *The function adds an item in localStorage. Like a parameter, accepts two arguments, the first argument  should be string, the second argument can be any kind of type.
+ *The function adds an item in localStorage. As a parameter, accepts two arguments, the first argument  should be string, the second argument can be any kind of type.
  * @param {string}-localStorage property key
  * @param {*}-localStorage property value
  * @returns {*}-property value
@@ -11,3 +11,24 @@ export function addItemToLocalStorage(key, value) {
 }
 
 
+/**
+ *The function gets the item from localStorage by key. As a parameter accepts one  argument- property key.
+ * @param {string}-localStorage property key
+ * @returns {object}-local storage property value
+ */
+
+export function getItemFromLocalStorage(key){
+  return JSON.parse(localStorage.getItem(`${key}`));
+}
+
+
+/**
+ *The function removes the item from localStorage by key. As a parameter accepts one argument- property key, which will be deleted.
+ * @param {string}-localStorage property key
+ * @returns {string}-localStorage property key
+ */
+
+export function removeItemFromLocalStorage(key){
+  localStorage.removeItem(`${key}`);
+  return key;
+}
--- app/src/services/getFavoriteCountries.js
+++ app/src/services/getFavoriteCountries.js
@@ -1,6 +1,7 @@
 import {addItemToLocalStorage} from "../helpers/localStorage.js"
+import {getItemFromLocalStorage} from "../helpers/localStorage.js"
 
 export function getFavoriteCountries(){ 
-    return localStorage.hasOwnProperty("favoriteCountries") ? JSON.parse(localStorage.getItem("favoriteCountries")) : addItemToLocalStorage("favoriteCountries", []) 
+    return localStorage.hasOwnProperty("favoriteCountries") ? getItemFromLocalStorage("favoriteCountries") : addItemToLocalStorage("favoriteCountries", []) 
 }
 
