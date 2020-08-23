8e2b7bf6e59b5157f237c3f62f4b382f57595334
Naira Yeghiazaryan <yeghiazaryannaira97@gmail.com>
2020-08-23 10:15:10 +0400

    some changes in country.css file

--- app/src/helpers/makeRowContent.js
+++ app/src/helpers/makeRowContent.js
@@ -3,6 +3,7 @@ export const  makeRowContent = (key, value) =>{
     const content = document.createElement("div");
     const contentKey = document.createElement("h3");
     const contentValue = document.createElement("p");
+    const decoration = document.createElement("hr")
     
     content.classList.add("grid-wrapper")
     contentKey.textContent = `${key}`;
@@ -12,6 +13,6 @@ export const  makeRowContent = (key, value) =>{
     contentValue.classList.add("value");
     content.appendChild(contentKey);
     content.appendChild(contentValue);
-  
+    content.appendChild(decoration)
     return content;
   }
