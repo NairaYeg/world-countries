cfacdc30d91735fb3bffe0875301a834e2eb7d0b
Naira Yeghiazaryan <yeghiazaryannaira97@gmail.com>
2020-08-23 19:12:00 +0400

    some fixes

--- dist/favorites.748fa525.js
+++ dist/favorites.748fa525.js
@@ -510,7 +510,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53476" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51775" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -687,4 +687,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","src/favorites.js"], null)
-//# sourceMappingURL=/favorites.748fa525.js.map
+//# sourceMappingURL=dist/favorites.748fa525.js.map
--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -589,7 +589,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53476" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51775" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -766,4 +766,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","src/index.js"], null)
-//# sourceMappingURL=/src.a2b27638.js.map
+//# sourceMappingURL=dist/src.a2b27638.js.map
